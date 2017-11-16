3d3e5a3e83a37242d6620338354c00ea9cbaa667
wy01750 <wangyue1@ly.com>
2017-11-16 14:59:45 +0800

    kafka design modify

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -10,7 +10,7 @@ use output;
 pub fn run() {
     let (tx, rx): (SyncSender<BytesMut>, Receiver<BytesMut>) = mpsc::sync_channel(100000);
 
-    output::kafka::poll_start(rx);
+    output::poll_start(rx);
 
     input::tcp::serve_frame(tx.clone());
 }
--- src/output/mod.rs
+++ src/output/mod.rs
@@ -1,2 +1,50 @@
 
+
+use std::sync::mpsc;
+use std::sync::mpsc::Receiver;
+
+use std::thread;
+use std::time::Duration;
+
+use bytes::BytesMut;
+
 pub mod kafka;
+pub mod rdkafka;
+
+pub fn poll_start(rx: Receiver<BytesMut>) {
+    thread::Builder::new()
+        .name("kafka-output".to_string())
+        .spawn( move || {
+            poll_with_multi_worker(rx, 5);
+        }).unwrap();
+}
+
+fn poll_with_multi_worker(rx :Receiver<BytesMut>, num_threads: i32) {
+    let recv_timeout_sec = 3;
+
+    let mut channels = Vec::new();
+    for n in 0..num_threads {
+        let (worker_tx, worker_rx) = mpsc::channel();
+        channels.push(worker_tx);
+        thread::Builder::new()
+            .name(format!("kafka-worker-{}", n).to_string())
+            .spawn(move || kafka::worker(worker_rx)).unwrap();
+    }
+
+    let mut channel_index = 0;
+    loop {
+        let rt = rx.recv_timeout(Duration::new(recv_timeout_sec, 0));
+
+        let index = if channel_index < num_threads {
+            channel_index as usize
+        } else {
+            channel_index = 0;
+            channel_index as usize
+        };
+
+        let worker_tx = &channels[index];
+        worker_tx.send(rt).unwrap();
+
+        channel_index = channel_index + 1;
+    }
+}
